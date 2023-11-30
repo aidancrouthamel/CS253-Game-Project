3954e8103ad143126b74c1a5b6f135cfe220c7d9
AlexCarollo <144820334+AlexCarollo@users.noreply.github.com>
2023-11-30 07:14:26 +0000

    Finished setting up initial flask server and HTML

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return render_template("main.html")
+def game_main():
+    return render_template("game_main.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0", port = 5000)
