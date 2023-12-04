d40fe22b3aeb466b493b1e1d467fa2ac89ad85eb
AlexCarollo <144820334+AlexCarollo@users.noreply.github.com>
2023-12-04 01:10:51 +0000

    Added a seperate page for leaderboards.

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
+## Main page = 0
+## Leaderboard page = 1
+current_page = 0
+
 @app.route("/")
 def game_main():
     return render_template("game_main.html")
+
+'''
+@app.route("/", methods=["POST"])
+def leaderboard():
+    global current_page
+    if current_page == 0:
+        current_page = 1
+        return render_template("full_leaderboard.html")
+    else:
+        current_page = 0
+        return render_template("game_main.html")
+
+'''
 
 
 if __name__ == "__main__":
