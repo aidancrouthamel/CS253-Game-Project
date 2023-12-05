4a0e7e9ab7a96ad372a7debc4e304bcf2606fd0f
AlexCarollo <144820334+AlexCarollo@users.noreply.github.com>
2023-12-05 16:23:48 +0000

    Minor changes to formatting.

--- app.py
+++ app.py
@@ -15,19 +15,6 @@ def game_main():
 def leaderboard():
     return render_template("full_leaderboard.html")
     
-'''
-@app.route("/", methods=["POST"])
-def leaderboard():
-    global current_page
-    if current_page == 0:
-        current_page = 1
-        return render_template("full_leaderboard.html")
-    else:
-        current_page = 0
-        return render_template("game_main.html")
-
-'''
-
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 5000)
