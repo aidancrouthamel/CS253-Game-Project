dbd4bc5990a21a112d25ac124a4e5f3c0afd5ef5
aidancrouthamel <aidan8n@gmail.com>
2023-12-12 11:41:50 -0600

    Adding fetch_total_score and fetch_total_games functions.

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def game_main():
 @app.route("/leaderboard")
 def leaderboard():
     scores = fetch_rows(5000)
-    return render_template("full_leaderboard.html", scores=scores)
+    total_score = fetch_total_score()
+    total_games = fetch_total_games()
+    return render_template("full_leaderboard.html", scores=scores, total_score=total_score, total_games=total_games)
 
 
 def fetch_rows(num_rows):
@@ -25,6 +27,32 @@ def fetch_rows(num_rows):
     for i in range(100):
         try:
             cursor.execute("SELECT user, score FROM user_scores ORDER BY score DESC LIMIT %s", (num_rows,))
+            rows = cursor.fetchall()
+            return rows
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+
+def fetch_total_score():
+    cursor = db.cursor()
+    for i in range(100):
+        try:
+            cursor.execute("SELECT SUM(score) FROM user_scores")
+            rows = cursor.fetchall()
+            return rows
+        except Exception as e:
+            print(e)
+            continue
+        break
+
+
+def fetch_total_games():
+    cursor = db.cursor()
+    for i in range(100):
+        try:
+            cursor.execute("SELECT COUNT(id) FROM user_scores")
             rows = cursor.fetchall()
             return rows
         except Exception as e:
