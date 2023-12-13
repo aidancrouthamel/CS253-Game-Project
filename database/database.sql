2b2e46112247b06a126cecf24b8d361a0565b7b2
aidancrouthamel <acroutha@iwu.edu>
2023-12-12 20:04:55 -0600

    Adding more data into database.sql.

--- database/database.sql
+++ database/database.sql
@@ -16,3 +16,14 @@ INSERT INTO user_scores (user, score) VALUES ("FOG", "12389");
 INSERT INTO user_scores (user, score) VALUES ("GOG", "1246");
 INSERT INTO user_scores (user, score) VALUES ("HII", "15609");
 INSERT INTO user_scores (user, score) VALUES ("ILL", "12");
+
+INSERT INTO user_scores (user, score) VALUES ("ABC", "12234");
+INSERT INTO user_scores (user, score) VALUES ("BCD", "3999");
+INSERT INTO user_scores (user, score) VALUES ("CDE", "1995");
+INSERT INTO user_scores (user, score) VALUES ("DEF", "2000");
+INSERT INTO user_scores (user, score) VALUES ("EFG", "400");
+INSERT INTO user_scores (user, score) VALUES ("FGH", "23232");
+INSERT INTO user_scores (user, score) VALUES ("GHI", "1238");
+INSERT INTO user_scores (user, score) VALUES ("HIJ", "1256");
+INSERT INTO user_scores (user, score) VALUES ("IJK", "1560");
+INSERT INTO user_scores (user, score) VALUES ("JKL", "1200");
